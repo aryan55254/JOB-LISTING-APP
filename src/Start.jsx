6682ea7c626373fe8348e9093b8fc30a9d5b0035
aryan55254 <aryanmishra55254@gmail.com>
2025-01-02 12:38:03 +0530

    secondlast c

---
+++ src/Start.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Start() {
+  return (
+    <>
+    <Navbar />
+    <Hero TITLE ="FIND YOUR DREAM JOB WITH JUST A CLICK" SUBTITLE ='FIND JOBS THAT MATCH YOUR SKILLSET AND NEEDS' />
+    <Card1 />
+    <Jobscard />
+    <Footer />
+    </>
+  )
+}
+
+export default Start
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,38 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import Jobscard from './components/Jobscard.jsx';
+import Footer from './components/Footer.jsx';
+import Alljobs from './Alljobs.jsx';
+import JobForm from './JobForm.jsx';
+import Start from './Start.jsx';
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          path: '/',
+          element: <Start />  
+        },
+        {
+          path: '/alljobs',
+          element: <Alljobs />
+        },
+        {
+          path: '/addjobs',
+          element: <JobForm />
+        }
+      ]
+    }
+  ]
+);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <>
-   <App />
+   <RouterProvider router = {router}/>
    </>
     
   </StrictMode>,
